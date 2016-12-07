aa91edbea45dffffcdfc70b3066e77f4fdef2a1a
link-coala <mpj183@aol.com>
2016-12-06 20:16:16 -0500

    initial

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("p1").innerHTML = "bienvenido aventurero, este es el momento indicado para volver a a los sitios que siempre has conocido con las personas que ya has visto antes";
+document.getElementById("p2").innerHTML = "decidir.";
+document.getElementById("si").innerHTML = "irme";
+document.getElementById("no").innerHTML = "quedarme";
+
+$(document).ready(function(){
+    $("a").mouseover(function(){
+    $("#p2").removeClass("see");
+    });
+    $("a").mouseout(function(){
+    $("#p2").addClass("see");
+    });
+});
